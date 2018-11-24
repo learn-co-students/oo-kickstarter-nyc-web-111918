a4eb4ec2f8d262a1b47debb408fb4869233238bf
oahtpham <phamthaop.9@gmail.com>
2018-11-23 22:29:09 -0500

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,21 @@
+class Project
+
+  attr_accessor :title, :backers
+
+  # @@backers = []
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  # def self.backers
+  #   @@backers
+  # end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+
+end
